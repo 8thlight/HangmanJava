afd520359380aa9d5f199042f28be352f059caba
Eric Smith <esmith@8thlight.com>
2014-09-21 20:52:38 -0500

    Hangman guessing now functionals.
    You also get clues back with a crude thing. I now need a way to output
    to the console and make sure the game plays.
    
    Check winner and loser as well. Will need fail count too.
    
    Also need a tester for to check the percentage wins and the average
    moves to win.
    
    Finally load guess from a file.

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -1,34 +1,49 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HangmanGame implements Game{
     public static int MaxGuesses = 5;
     private List<Character> answer;
-    private List<Character> clue;
+    private List<Character> guesses;
 
     public HangmanGame()
     {
-        clue = new ArrayList<Character>();
+        guesses = new ArrayList<Character>();
     }
 
     @Override
     public boolean IsOver()
     {
-        return (clue.size() >= MaxGuesses) || this.clue.containsAll(this.answer);
+        return (guesses.size() >= MaxGuesses) || this.guesses.containsAll(this.answer);
     }
 
     @Override
     public void Guess(char guess)
     {
-        clue.add(guess);
+        guesses.add(Character.toLowerCase(guess));
     }
 
     @Override
-    public List<Character> CurrentClue() {
-        return null;
+    public List<Character> CurrentClue()
+    {
+        List<Character> clue = new ArrayList<Character>();
+
+        for (int i = 0; i < answer.size(); i++) {
+            Character c = answer.get(i);
+            if (guesses.contains(Character.toLowerCase(c)))
+                clue.add(c);
+            else
+                clue.add('_');
+        }
+
+        return clue;
     }
 
-    public void SetClue(List<Character> answer) {
-        this.answer = answer;
+    public void SetAnswer(String answer) {
+        this.answer = new ArrayList<Character>();
+        for (Character c : answer.toCharArray()) {
+            this.answer.add(c);
+        }
     }
 }
--- src/test/java/HangmanGameTest.java
+++ src/test/java/HangmanGameTest.java
@@ -8,7 +8,7 @@ public class HangmanGameTest {
     public void ItIsNotOverWhenTheMaxGuessesAreNotUsedYet() {
         HangmanGame game = new HangmanGame();
 
-        game.SetClue(Arrays.asList('a','b'));
+        game.SetAnswer("ab");
         for(int i=0; i < HangmanGame.MaxGuesses; i++) {
             assertFalse(game.IsOver());
             game.Guess('c');
@@ -18,7 +18,7 @@ public class HangmanGameTest {
     @Test
     public void ItIsOverWhenTheMaxGuessesAreUsed() {
         HangmanGame game = new HangmanGame();
-        game.SetClue(Arrays.asList('a','b'));
+        game.SetAnswer("ab");
 
         for(int i=0; i < HangmanGame.MaxGuesses; i++) {
             game.Guess('c');
@@ -30,11 +30,79 @@ public class HangmanGameTest {
     @Test
     public void ItIsOverWhenTheAnswerIsGuessed() {
         HangmanGame game = new HangmanGame();
-        game.SetClue(Arrays.asList('a', 'b'));
+        game.SetAnswer("ab");
 
         game.Guess('a');
         game.Guess('b');
 
         assertTrue(game.IsOver());
+    }
+
+    @Test
+    public void TheAnswerCanBeGuessedInTheWrongOrder() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("ab");
+
+        game.Guess('b');
+        game.Guess('a');
+
+        assertTrue(game.IsOver());
+    }
+
+    @Test
+    public void TheAnswerCanBeGuessedWhenThereAreDuplicates() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("abbb");
+
+        game.Guess('b');
+        game.Guess('a');
+
+        assertTrue(game.IsOver());
+    }
+
+    @Test
+    public void TheCurrentClueStartsAsAListOfEmptySlots() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("eric");
+
+        assertEquals(Arrays.asList('_', '_', '_', '_'), game.CurrentClue());
+    }
+
+    @Test
+    public void TheCurrentClueGetsACorrectGuess() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("eric");
+        game.Guess('e');
+
+        assertEquals(Arrays.asList('e', '_', '_', '_'), game.CurrentClue());
+    }
+
+    @Test
+    public void TheCurrentClueCanHandleMoreThanOneCorrectGuess() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("eric");
+        game.Guess('e');
+        game.Guess('i');
+
+        assertEquals(Arrays.asList('e', '_', 'i', '_'), game.CurrentClue());
+    }
+
+    @Test
+    public void TheCurrentClueAlsoHasRepeatingLetters() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("jimmy");
+        game.Guess('m');
+
+        assertEquals(Arrays.asList('_', '_', 'm', 'm', '_'), game.CurrentClue());
+    }
+
+    @Test
+    public void CaseIsIrrelevant() {
+        HangmanGame game = new HangmanGame();
+        game.SetAnswer("PLEASE");
+
+        game.Guess('p');
+
+        assertEquals(Arrays.asList('P', '_', '_', '_', '_', '_'), game.CurrentClue());
     }
 }
