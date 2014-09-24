1c1351604ceea5bbe66df94752c0d18510719287
Anton Perez <antonperez@livenation.com>
2014-09-24 11:30:17 -0700

    Able to set and assert on all guesses sent in.

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,33 @@
 package com.hangman.players;
 import com.hangman.Player;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
-  @Override
-  public char GetGuess(List<Character> clue) {
-    return 'a';
-  }
+    private LinkedList<Character> guesses;
+    private List<Character> latestClue;
+
+    public YourPlayer() {
+        guesses = new LinkedList<Character>();
+    }
+
+    public void SetGuesses(List<Character> chars) {
+        this.guesses = new LinkedList<Character>();
+        for(char guess : chars) {
+            this.guesses.add(guess);
+        }
+    }
+
+    @Override
+    public char GetGuess(List<Character> clue) {
+        latestClue = clue;
+        if (guesses.size() > 0)
+            return guesses.pop();
+        return 0;
+    }
+
+    public List<Character> GetLatestClue() {
+        return latestClue;
+    }
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,27 @@
 package com.hangman.players;
 
 import org.junit.Test;
+
 import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
     @Test
-    public void AlwaysGuessA() {
+    public void alwaysGuessA() {
         YourPlayer player = new YourPlayer();
-
+        player.SetGuesses(Arrays.asList('a', 'b', 'c'));
         char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
 
         assertEquals('a', guess);
     }
+
+    @Test
+    public void setsAllGuessed() throws Exception {
+        YourPlayer player = new YourPlayer();
+        player.SetGuesses(Arrays.asList('a', 'b', 'c'));
+
+        assertEquals(Arrays.asList('a', 'b', 'c'), player.GetLatestClue());
+    }
+
 }
