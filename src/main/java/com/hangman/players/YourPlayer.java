e2af788f558f6398a69989d605a0d0966c1979da
Jonathan Palace <jonathanpalace@livenation.com>
2014-09-24 11:07:46 -0700

    guess first 11 letters

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,31 @@
 package com.hangman.players;
 import com.hangman.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+
+  private ArrayList<Character> alphabet = new ArrayList();
+
+  public YourPlayer(){
+    alphabet.add('a');    alphabet.add('b');    alphabet.add('c');    alphabet.add('d');    alphabet.add('e');
+    alphabet.add('f');    alphabet.add('g');    alphabet.add('h');    alphabet.add('i');    alphabet.add('j');
+    alphabet.add('k');    alphabet.add('l');    alphabet.add('m');    alphabet.add('n');    alphabet.add('o');
+    alphabet.add('p');    alphabet.add('q');    alphabet.add('r');    alphabet.add('s');    alphabet.add('t');
+    alphabet.add('u');    alphabet.add('v');    alphabet.add('w');    alphabet.add('x');    alphabet.add('y');
+    alphabet.add('z');
+
+  }
+
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+    //clue is where you are currently at in the game
+
+    char letterGuess = alphabet.remove(0);
+
+    return letterGuess;
   }
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,6 +1,8 @@
 package com.hangman.players;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
@@ -9,8 +11,31 @@ public class YourPlayerTest {
     public void AlwaysGuessA() {
         YourPlayer player = new YourPlayer();
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+      char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
 
         assertEquals('a', guess);
     }
+
+  @Test
+  public void GuessAThenB() {
+    YourPlayer player = new YourPlayer();
+    char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+    assertEquals('a', guess);
+    guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+    assertEquals('b', guess);
+  }
+
+  //not Testing yet
+  public void goodLetters(){
+    YourPlayer player = new YourPlayer();
+
+    char guess = player.GetGuess(Arrays.asList('r','s','t','l','n','e'));
+
+    assertEquals('r', guess);
+    assertEquals('s', guess);
+    assertEquals('t', guess);
+    assertEquals('l', guess);
+    assertEquals('n', guess);
+    assertEquals('e', guess);
+  }
 }
