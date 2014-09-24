c81e9e39acdf4eb1291445e2fddd1b585c7f262d
Daniel Arlitt <daniel.arlitt@ticketmaster.com>
2014-09-24 11:25:16 -0700

    Start testing best guesses

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,31 @@
 package com.hangman.players;
 import com.hangman.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+  List<Character> guesses;
+  List<Character> best = Arrays.asList('e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r');
+
+  public YourPlayer() {
+      guesses = new ArrayList<Character>();
+  }
+
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+    char guess = 'e';
+
+    for (char g: guesses) {
+        if (g == 'e') {
+             guess = 't';
+            break;
+        }
+    }
+
+    guesses.add(guess);
+    return guess;
   }
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,36 @@
 package com.hangman.players;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
-    @Test
-    public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
+    private YourPlayer player;
 
+    @Before
+    public void setup() {
+        player = new YourPlayer();
+    }
+
+    @Test
+    public void AlwaysGuessE() {
         char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
 
-        assertEquals('a', guess);
+        assertEquals('e', guess);
     }
+
+    @Test
+    public void ifEIsAlreadyGuessedGuessT() {
+        char guess1 = player.GetGuess(Arrays.asList('_', '_', '_'));
+
+        assertEquals('e', guess1);
+
+
+        char guess2 = player.GetGuess(Arrays.asList('_', '_', '_'));
+
+        assertEquals('t', guess2);
+    }
+
+
 }
