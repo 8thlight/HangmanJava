6af5cd986fdac0ef533c47298bdfd6356ca483a4
deansacramone <dean.sacramone@ticketmaster.com>
2014-09-24 11:54:31 -0700

    latest

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -4,10 +4,30 @@ import com.hangman.Player;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class YourPlayer implements Player {
-  @Override
-  public char GetGuess(List<Character> clue) {
-    return 'a';
-  }
+    char[] totalGuesses = new char[9];
+    private int indx = 0;
+
+    @Override
+    public char GetGuess(List<Character> clue) {
+        List<Character> clues  = clue;
+        char guessChar  = returnRandomVal(clues);
+        totalGuesses[indx]= guessChar;
+        indx++;
+        return guessChar;
+
+    }
+
+    public char returnRandomVal( List<Character> clue ){
+        Random random = new Random();
+        int val = random.nextInt(clue.size());
+        return clue.get(val);
+    }
+
+
+    public  char[] returnPreviousGuesses(){
+        return totalGuesses;
+    }
 }
