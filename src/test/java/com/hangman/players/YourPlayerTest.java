bd66530ee2d37f1270468490e6fd50defacc7ca3
Jonathan Alviar <jonathan.alviar@ticketmaster.com>
2014-09-24 11:54:26 -0700

    Push to master

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,42 @@
 package com.hangman.players;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
-    @Test
-    public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+  YourPlayer player;
 
-        assertEquals('a', guess);
-    }
+  @Before
+  public void setup() {
+    player = new YourPlayer();
+  }
+
+  @Test
+  public void AlwaysGuessA() {
+    char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+
+    assertEquals('a', guess);
+  }
+
+  @Test
+  public void CheckForGuessedAnswers() throws Exception
+  {
+    LinkedList<Character> guesses = new LinkedList<Character>(Arrays.asList('a', 'b', 'c', 'a', 'b'));
+    LinkedList guessList = new LinkedList();
+
+    player.GetGuess(guesses);
+    guessList = player.getGuesses();
+
+//    assertEquals(5, guessList.size());
+
+
+  }
+
+
 }
