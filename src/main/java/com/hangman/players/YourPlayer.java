56ce00c3cf8acba343e6c0fa1a83ad8e92cbeb44
Wan Chi Chio <wanchi.chio@usholwch-ml.lyv.livenation.com>
2014-09-24 11:17:40 -0700

    Add some functionalities on guessing character

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -2,12 +2,34 @@ package com.hangman.players;
 import com.hangman.Player;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+
+  HashMap<Character, Integer> guessedCharacters;
+
+  public YourPlayer() {
+      guessedCharacters = new HashMap<Character, Integer>();
+  }
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+
+      char guess = randomCharacter();
+
+      for(int i=0; i < clue.size(); i++) {
+          if (guess == clue.get(i)) guess = randomCharacter();
+      }
+
+      guessedCharacters.put(guess, 1);
+
+      return guess;
+
   }
+
+  public char randomCharacter() {
+      return (char) (0 + (int)(Math.random()*25) + 97);
+  }
+
 }
