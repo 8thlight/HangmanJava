c85106309ced63aeb5af915d7edfafd575497ab5
Charles <zcz958@gmail.com>
2014-09-24 11:56:00 -0700

    Charles

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -5,12 +5,18 @@ import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
-    @Test
-    public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
+  @Test
+  public void AlwaysGuessA() {
+    YourPlayer player = new YourPlayer();
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+    char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+    assertEquals('e', guess);
+  }
 
-        assertEquals('a', guess);
-    }
+  @Test
+  public void AlwaysGuessD() {
+    YourPlayer player = new YourPlayer();
+    char guess = player.GetGuess(Arrays.asList('a', 'b'));
+    assertEquals('i', guess);
+  }
 }
