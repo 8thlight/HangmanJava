e31d5dde72c500f5add373ad1105bd60300a1e6b
Michael Bochkaryov <MichaelBochkaryov@livenation.com>
2014-11-12 14:37:01 -0800

    Initial implementation with minimal testcases

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,29 @@
 package com.hangman.players;
-import com.hangman.Player;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
+import com.hangman.Player;
+
 public class YourPlayer implements Player {
-  @Override
-  public char GetGuess(List<Character> clue) {
-    return 'a';
-  }
+
+	private ArrayDeque<Character> dict;
+
+	public YourPlayer() {
+		this.dict = new ArrayDeque<Character>();
+		dict.addAll(Arrays.asList('e','t','a','o','i','n','s','h','r','d','l','c','u','m','w','f','g','y','p','b','v','k','j','x','q','z'));
+	}
+
+	@Override
+	public char GetGuess(List<Character> clue) {
+
+		Character nextGuess;
+		do {
+			nextGuess = dict.pop();
+		} while (clue.contains(nextGuess));
+
+		return nextGuess;
+
+	}
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,34 @@
 package com.hangman.players;
 
-import org.junit.Test;
-import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
+import org.junit.Test;
+
 public class YourPlayerTest {
+
     @Test
-    public void AlwaysGuessA() {
+    public void ReturnSequentially() {
         YourPlayer player = new YourPlayer();
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
-
+        char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertEquals('e', guess);
+        guess = player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertEquals('t', guess);
+        guess = player.GetGuess(Arrays.asList('_', '_', '_'));
         assertEquals('a', guess);
     }
+
+    @Test
+    public void NotReturnIfPresent() {
+        YourPlayer player = new YourPlayer();
+
+        char guess = player.GetGuess(Arrays.asList('_', '_', 't'));
+        assertEquals('e', guess);
+
+        guess = player.GetGuess(Arrays.asList('_', '_', 't'));
+        assertEquals('a', guess);
+    }
+ 
 }
