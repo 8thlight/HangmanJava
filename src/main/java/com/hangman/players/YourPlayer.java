48eae905c878ef42fdb86bc90fb103ccc9901bac
Evelina Lurye <Evelina.Lurye@ticketmaster.com>
2014-11-12 15:06:17 -0800

    Evelina message

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,41 @@
 package com.hangman.players;
+
 import com.hangman.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+
+    private ArrayList<Character> guessed = new ArrayList<Character>();
+    private int count = 0;
+
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+
+      List<Character> charsList = Arrays.asList('a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+
+
+      for(Character letter: charsList) {
+          if(clue.contains(letter)) {
+
+              guessed.add(letter);
+              count++;
+              break;
+          }
+      }
+
+      char guess;
+      if(count>0)
+          guess = guessed.get(count-1);
+      else
+          guess = 'a';
+      return guess;
+
+  }
+
+  public ArrayList<Character> GuessedAlready() {
+      return guessed;
   }
 }
