f234672dfe0d0efe9a6bb47c671c6d18c342d678
sapana gupta <sapana.gupta@ticketmaster.com>
2014-11-12 15:12:13 -0800

    SA

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,31 @@
 package com.hangman.players;
 
+import org.junit.Before;
 import org.junit.Test;
+
 import java.util.Arrays;
+import java.util.LinkedList;
+
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
-    @Test
-    public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+	@Test 
+	public void alwaysGuessA() throws Exception  { 
+       YourPlayer player = new YourPlayer(); 
+       player.setGuesses(Arrays.asList('a', 'b', 'c')); 
+       char guess = player.GetGuess(Arrays.asList('a', 'b', 'c')); 
+       assertEquals('a', guess); 
+   } 
 
-        assertEquals('a', guess);
-    }
-}
+ @Test 
+ public void setsAllGuessed() throws Exception { 
+     YourPlayer player = new YourPlayer(); 
+     player.setGuesses(Arrays.asList('a', 'b', 'c')); 
+     player.GetGuess(Arrays.asList('a', 'b', 'c')); 
+
+     assertEquals(Arrays.asList('a', 'b', 'c'), player.getLatestClue()); 
+ } 
+
+} 
+
