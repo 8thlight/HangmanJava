0205c9e2745f1a12c06160d3778f8adbbe65f307
George Yue <george.yue@ticketmaster.com>
2014-11-12 14:09:06 -0800

    init try

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,66 @@
 package com.hangman.players;
 import com.hangman.Player;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class YourPlayer implements Player {
+
+  public List<Character> guessed = new ArrayList<Character>();
+  private ArrayList<Character> availableCharacters = new ArrayList<Character>();
+
+  public YourPlayer() {
+    String str = "abcdefghijklmnopqrstuvwxyz";
+
+    for(Character c : str.toCharArray()) {
+      availableCharacters.add(c);
+    }
+  }
+
+  public void buildGuessed(List<Character> clue) {
+    for (Character c : clue) {
+      if (!c.equals('_') && guessed.indexOf(c) == -1) {
+        guessed.add(c);
+      }
+    }
+  }
+
+  public List<Character> getGuessed() {
+    return guessed;
+  }
+
+  public Character gimmeACharacter() {
+    if (availableCharacters.size() < 1)
+      return 'a';
+
+    Character c = availableCharacters.remove( randInt() );
+
+    while (availableCharacters.size() > 0 && guessed.indexOf(c) != -1) {
+      c = availableCharacters.remove( randInt() );
+    }
+
+    guessed.add(c);
+
+    return c;
+  }
+
+  
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+    buildGuessed(clue);
+    return gimmeACharacter();
+  }
+
+  // http://stackoverflow.com/questions/363681/generating-random-integers-in-a-range-with-java
+  public int randInt() {
+
+    int min = 1;
+    int max = availableCharacters.size();
+
+    return rand(min, max);
+  }
+
+  public int rand(int min, int max) {
+    Random random = new Random();
+    return (random.nextInt(max - min + 1) + min);
   }
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,67 @@
 package com.hangman.players;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 public class YourPlayerTest {
-    @Test
-    public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+  public YourPlayer player;
 
-        assertEquals('a', guess);
-    }
+  @Before
+  public void setup() {
+    player = new YourPlayer();
+  }
+
+  @Test
+  public void buildGuessedList() {
+    ArrayList<Character> c = new ArrayList<Character>();
+
+    c.add('a');
+
+    player.buildGuessed(c);
+
+    assertEquals(c, player.getGuessed());
+  }
+
+  @Test
+  public void doesNotIncludeUnderscoreInGuessed() {
+    player.buildGuessed(Arrays.asList('_'));
+
+    List<Character> l = player.getGuessed();
+
+    assertEquals(-1, l.indexOf('_'));
+  }
+
+
+  @Test
+  public void shouldGetCharacter() {
+    Character c = player.gimmeACharacter();
+    assertNotNull(c);
+  }
+
+  @Test
+  public void shouldGetCharacterNotGuessed() {
+    player.GetGuess(Arrays.asList('_'));
+
+    Character c = player.gimmeACharacter();
+
+    assertNotNull(c);
+  }
+
+//  @Test
+//  public void AlwaysGuessA() {
+//    YourPlayer player = new YourPlayer();
+//
+//    char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+//
+//    assertEquals('a', guess);
+//  }
 }
