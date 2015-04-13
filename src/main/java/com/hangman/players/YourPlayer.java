ec41e448b58aaf5a18db65a7cfe8b91cd3d75cc3
mconklin <mark_conklin@surveysampling.com>
2015-04-13 12:12:32 -0400

    broken

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,38 @@
 package com.hangman.players;
 import com.hangman.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+
+  public static char[] vowels = {'a', 'e', 'i', 'o', 'u', 'y'};
+
   @Override
   public char GetGuess(List<Character> currentClue) {
+
+     List gueses = new ArrayList<String>();
+
+    for(char vowel : vowels)
+    {
+      if (currentClue.contains(vowel))
+      {
+        return 'z'; //constanant
+      }
+
+      if (!gueses.contains(String.valueOf(vowel)))
+      {
+        gueses.add(String.valueOf(vowel));
+        return vowel;
+      }
+
+    }
+
     return 'a';
+
   }
+
+
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,12 +1,18 @@
 package com.hangman.players;
 
+
 import org.junit.Test;
+
+
 import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 
 public class YourPlayerTest {
     @Test
-    public void GuessesAWhenThereAreNoSuccessfulCharactersGuessedYet() {
+    public void Guesses_A_When_ThereAreNoSuccessfulCharactersGuessedYet() {
         YourPlayer player = new YourPlayer();
 
         char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
@@ -14,8 +20,10 @@ public class YourPlayerTest {
         assertEquals('a', guess);
     }
 
+
+
     @Test
-    public void GuessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotA() {
+    public void Guesses_A_When_There_Are_Successful_Characters_Guessed_That_Are_Not_A() {
         YourPlayer player = new YourPlayer();
 
         char guess = player.GetGuess(Arrays.asList('m', '_', 'n'));
@@ -24,11 +32,29 @@ public class YourPlayerTest {
     }
 
     @Test
-    public void GuessesAWhenAIsThereAreAsInTheClueAsWell() {
+    public void Guess_z_when_vowel_is_in_answer() {
         YourPlayer player = new YourPlayer();
-
         char guess = player.GetGuess(Arrays.asList('_', 'a', '_'));
+        assertEquals('z', guess);
 
-        assertEquals('a', guess);
+    }
+
+    @Test
+    public void Guess_Vowels_Until_a_Vowel_is_Selected()
+    {
+        YourPlayer player = new YourPlayer();
+        char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertTrue(Arrays.asList(player.vowels).contains(guess));
+        char guess2 = player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertTrue(Arrays.asList(player.vowels).contains(guess2));
+    }
+
+    @Test
+    public void Should_Not_Guess_Same_guess_twice()
+    {
+        YourPlayer player = new YourPlayer();
+        char intitailGuess = player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertTrue(String.valueOf(intitailGuess).equals(String.valueOf('a')));
+        assertFalse(String.valueOf('a').equals(String.valueOf(player.GetGuess(Arrays.asList('_', '_', '_')))));
     }
 }
