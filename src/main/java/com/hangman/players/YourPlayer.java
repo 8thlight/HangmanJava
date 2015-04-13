5f2d160a7006480496abdf0aef23c911b089c13a
Austin Moran <austin.moran@surveysampling.com>
2015-04-13 11:58:11 -0400

    start implementation

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,43 @@
 package com.hangman.players;
+
 import com.hangman.Player;
 
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class YourPlayer implements Player {
-  @Override
-  public char GetGuess(List<Character> currentClue) {
-    return 'a';
-  }
+
+    private static final char[] LETTERS = {
+            'e', 't', 'a', 'o', 'i', 'n',
+            's', 'h', 'r', 'd', 'l', 'c',
+            'u', 'm', 'y', 'f', 'g', 'y',
+            'p', 'b', 'v', 'k', 'j', 'x',
+            'q', 'z'
+    };
+
+    List<Character> incorrectGuesses = new ArrayList<>();
+
+    @Override
+    public char GetGuess(List<Character> currentClue) {
+        return guessNextLetter();
+    }
+
+    private char guessNextLetter() {
+        return saveAndGuess(LETTERS[incorrectGuesses.size()]);
+    }
+
+    private int getRemainingBlanks(List<Character> currentClue) {
+        int blanks = 0;
+        for (Character c : currentClue) {
+            if (c.equals('_')) {
+                ++blanks;
+            }
+        }
+        return blanks;
+    }
+
+    private char saveAndGuess(char c) {
+        incorrectGuesses.add(c);
+        return c;
+    }
 }
