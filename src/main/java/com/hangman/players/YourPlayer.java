179f0f8a84c3531798b2eedb4e3d393b408122b2
Jyotsna Bhonde <jyotsna.bhonde@surveysampling.com>
2015-04-13 11:58:48 -0400

    Jyotsna's commit

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -8,6 +8,106 @@ import java.util.List;
 public class YourPlayer implements Player {
   @Override
   public char GetGuess(List<Character> currentClue) {
-    return 'a';
+    if (moreVowelsToBeReturned(currentClue)){
+      return nextVowelToReturn(currentClue);
+    }
+
+    return returnNextPopularConsonant(currentClue);
   }
+
+  public boolean moreVowelsToBeReturned(List<Character> currentClue){
+    List<Character> list = Arrays.asList('a', 'e', 'i', 'o', 'u', 'y');
+    if (currentClue.containsAll(list)){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  public char nextVowelToReturn(List<Character> currentClue){
+    if (!currentClue.contains('a')){
+      return 'a';
+    }
+    if (!currentClue.contains('e')){
+      return 'e';
+    }
+    if (!currentClue.contains('i')){
+      return 'i';
+    }
+    if (!currentClue.contains(('o'))){
+      return 'o';
+    }
+    if (!currentClue.contains('u')) {
+      return 'u';
+    }
+    if (!currentClue.contains('y')) {
+      return 'y';
+    }
+    throw new IllegalStateException("All vowels existed. No more vowels remain to be returned");
+  }
+
+  public char returnNextPopularConsonant(List<Character> currentClue){
+    if (!currentClue.contains('s')){
+      return 's';
+    }
+    if (!currentClue.contains('t')){
+      return 't';
+    }
+    if (!currentClue.contains('r')){
+      return 'r';
+    }
+    if (!currentClue.contains('l')){
+      return 'l';
+    }
+    if(!currentClue.contains('n')){
+      return 'n';
+    }
+    if(!currentClue.contains('p')){
+      return 'p';
+    }
+    if(!currentClue.contains('g')){
+      return 'g';
+    }
+    if(!currentClue.contains('h')){
+      return 'h';
+    }
+    if(!currentClue.contains('z')){
+      return 'z';
+    }
+    if(!currentClue.contains('m')){
+      return 'm';
+    }
+    if(!currentClue.contains('x')){
+      return 'x';
+    }
+    if(!currentClue.contains('c')){
+      return 'c';
+    }
+    if(!currentClue.contains('d')){
+      return 'd';
+    }
+    if(!currentClue.contains('b')){
+      return 'b';
+    }
+    if(!currentClue.contains('f')){
+      return 'f';
+    }
+    if(!currentClue.contains('j')){
+      return 'j';
+    }
+    if(!currentClue.contains('k')){
+      return 'k';
+    }
+    if(!currentClue.contains('q')){
+      return 'q';
+    }
+    if(!currentClue.contains('v')){
+      return 'v';
+    }
+    if(!currentClue.contains('w')){
+      return 'w';
+    }
+    throw new IllegalStateException("All consonants exhausted.");
+  }
+
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -2,6 +2,8 @@ package com.hangman.players;
 
 import org.junit.Test;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
@@ -24,11 +26,57 @@ public class YourPlayerTest {
     }
 
     @Test
-    public void GuessesAWhenAIsThereAreAsInTheClueAsWell() {
+    public void DoNotGuessAIfAlreadyGuessed(){
         YourPlayer player = new YourPlayer();
 
         char guess = player.GetGuess(Arrays.asList('_', 'a', '_'));
 
-        assertEquals('a', guess);
+        assertEquals('e', guess);
     }
+
+    @Test
+    public void ReturnsTrueIfMoreVowelsToBeReturned(){
+        YourPlayer player = new YourPlayer();
+        List<Character> currentClue = Arrays.asList('a', 'e', 'i', '_', 'o');
+        boolean result = player.moreVowelsToBeReturned(currentClue);
+
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void ReturnsFalseIfMoreVowelsToBeReturned(){
+        YourPlayer player = new YourPlayer();
+        List<Character> currentClue = Arrays.asList('a', 'e', 'i', '_', 'o', 'u', 'y');
+        boolean result = player.moreVowelsToBeReturned(currentClue);
+
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void GetNextVowelInList(){
+        YourPlayer player = new YourPlayer();
+        List<Character> currentClue = Arrays.asList('a', '_', '_', 'i');
+
+        char result = player.nextVowelToReturn(currentClue);
+
+        assertEquals('e', result);
+    }
+
+    @Test (expected = IllegalStateException.class)
+    public void ThrowsExceptionWhenNoMoreVowelsToBeReturned(){
+        YourPlayer player = new YourPlayer();
+        List<Character> currentClue = Arrays.asList('a', 'e', 'i', '_', 'o', 'u', 'y');
+        player.nextVowelToReturn(currentClue);
+    }
+
+    @Test
+    public void ReturnConsonantInOrderOfPopularity(){
+        YourPlayer player = new YourPlayer();
+        List<Character> currentClue = Arrays.asList('a', 'e', 'i', '_', 'o', 'u', 'y');
+
+        char guess = player.GetGuess(currentClue);
+
+        assertEquals('s', guess);
+    }
+
 }
