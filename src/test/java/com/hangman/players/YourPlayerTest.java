0c1b5c684c3b7b870fda0eddbd27189a8ddba551
Erik Paulsen <erik.paulsen@surveysampling.com>
2015-04-13 11:57:54 -0400

    Crummy start

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,34 +1,48 @@
 package com.hangman.players;
 
-import org.junit.Test;
+import org.junit.*;
 import java.util.Arrays;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
+import com.hangman.Player;
 
 public class YourPlayerTest {
+    Player player;
+
+    @Before
+    public void setup() {
+        player = new YourPlayer();
+    }
     @Test
     public void GuessesAWhenThereAreNoSuccessfulCharactersGuessedYet() {
-        YourPlayer player = new YourPlayer();
 
         char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
 
         assertEquals('a', guess);
     }
 
+//    @Test
+//    public void GuessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotA() {
+//
+//        char guess = player.GetGuess(Arrays.asList('m', '_', 'n'));
+//
+//        assertEquals('a', guess);
+//    }
+
+
     @Test
-    public void GuessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotA() {
-        YourPlayer player = new YourPlayer();
-
-        char guess = player.GetGuess(Arrays.asList('m', '_', 'n'));
-
-        assertEquals('a', guess);
+    public void ShouldTryAllVowelsWhenNoMatch() {
+        List<Character> noMatch = Arrays.asList('_', '_', '_');
+        assertEquals('a',  player.GetGuess(noMatch));
+        assertEquals('e',  player.GetGuess(noMatch));
+        assertEquals('i',  player.GetGuess(noMatch));
+        assertEquals('o',  player.GetGuess(noMatch));
+        assertEquals('u',  player.GetGuess(noMatch));
     }
 
     @Test
-    public void GuessesAWhenAIsThereAreAsInTheClueAsWell() {
-        YourPlayer player = new YourPlayer();
-
-        char guess = player.GetGuess(Arrays.asList('_', 'a', '_'));
-
-        assertEquals('a', guess);
+    public void ShouldTryConsonentsWhenTwoMatches() {
+        List<Character> noMatch = Arrays.asList('_', 'a', '_', 'o', '_');
+        assertEquals('s', player.GetGuess(noMatch));
     }
 }
