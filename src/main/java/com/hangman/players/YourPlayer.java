6be3d9d21d873d662773997359f1378553c8174e
zach_lysobey <zach@lysobey.com>
2015-04-13 11:21:11 -0400

    use most common english letters

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -9,34 +9,14 @@ public class YourPlayer implements Player {
     Queue<Character> bestGuesses = new LinkedList<Character>();
 
     public YourPlayer() {
-        bestGuesses.add('r');
-        bestGuesses.add('s');
-        bestGuesses.add('t');
-        bestGuesses.add('l');
-        bestGuesses.add('n');
-        bestGuesses.add('e');
-        bestGuesses.add('a');
-        bestGuesses.add('b');
-        bestGuesses.add('c');
-        bestGuesses.add('d');
-        bestGuesses.add('f');
-        bestGuesses.add('g');
-        bestGuesses.add('h');
-        bestGuesses.add('i');
-        bestGuesses.add('j');
-        bestGuesses.add('k');
-        bestGuesses.add('l');
-        bestGuesses.add('m');
-        bestGuesses.add('n');
-        bestGuesses.add('o');
-        bestGuesses.add('p');
-        bestGuesses.add('q');
-        bestGuesses.add('u');
-        bestGuesses.add('v');
-        bestGuesses.add('w');
-        bestGuesses.add('x');
-        bestGuesses.add('y');
-        bestGuesses.add('z');
+        setBestGuesses();
+    }
+
+    private void setBestGuesses() {
+        String bestGuessesString = "etaoinshrdlucmfwypvbgkqjxz";
+        for(char c : bestGuessesString.toCharArray()) {
+            bestGuesses.add(c);
+        }
     }
 
     @Override
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -13,63 +13,47 @@ public class YourPlayerTest {
     }
 
     @Test
-    public void GuessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotR() {
+    public void GuessesRWhenThereAreSuccessfulCharactersGuessedThatAreNotR() {
         YourPlayer player = new YourPlayer();
-        char guess = player.GetGuess(Arrays.asList('m', '_', 'n'));
+        char guess = player.GetGuess(Arrays.asList('r', '_', 'r'));
         assertEquals('r', guess);
     }
 
     @Test
-    public void DoesNotGuessAWhenAIsInTheClueAsWell() {
+    public void DoesNotGuessRWhenAIsInTheClueAsWell() {
         YourPlayer player = new YourPlayer();
-        char guess = player.GetGuess(Arrays.asList('_', 'r', '_'));
+        char guess = player.GetGuess(Arrays.asList('_', 'e', '_'));
         assertEquals('r', guess);
     }
 
     @Test
-    public void WillAnswerWithRSTLNEInOrder() {
+    public void WillAnswerWithMostCommonLetters() {
         YourPlayer player = new YourPlayer();
-        char guess1 = player.GetGuess(Arrays.asList('_'));
-        assertEquals('r', guess1);
-        char guess2 = player.GetGuess(Arrays.asList('_'));
-        assertEquals('s', guess2);
-        char guess3 = player.GetGuess(Arrays.asList('_'));
-        assertEquals('t', guess3);
-        char guess4 = player.GetGuess(Arrays.asList('_'));
-        assertEquals('l', guess4);
-        char guess5 = player.GetGuess(Arrays.asList('_'));
-        assertEquals('n', guess5);
-        char guess6 = player.GetGuess(Arrays.asList('_'));
-        assertEquals('e', guess6);
-    }
-
-    @Test
-    public void WillAnswerAlphabeticallyAfterTheFirst6Attempts() {
-        YourPlayer player = new YourPlayer();
-        for (int i = 0; i < 6; i++) {
-            player.GetGuess(Arrays.asList('_'));
-        }
+        nextGuessIs(player, 'e');
+        nextGuessIs(player, 't');
         nextGuessIs(player, 'a');
-        nextGuessIs(player, 'b');
-        nextGuessIs(player, 'c');
-        nextGuessIs(player, 'd');
-        nextGuessIs(player, 'f');
-        nextGuessIs(player, 'g');
-        nextGuessIs(player, 'h');
-        nextGuessIs(player, 'i');
-        nextGuessIs(player, 'j');
-        nextGuessIs(player, 'k');
-        nextGuessIs(player, 'l');
-        nextGuessIs(player, 'm');
-        nextGuessIs(player, 'n');
         nextGuessIs(player, 'o');
-        nextGuessIs(player, 'p');
-        nextGuessIs(player, 'q');
+        nextGuessIs(player, 'i');
+        nextGuessIs(player, 'n');
+        nextGuessIs(player, 's');
+        nextGuessIs(player, 'h');
+        nextGuessIs(player, 'r');
+        nextGuessIs(player, 'd');
+        nextGuessIs(player, 'l');
         nextGuessIs(player, 'u');
-        nextGuessIs(player, 'v');
+        nextGuessIs(player, 'c');
+        nextGuessIs(player, 'm');
+        nextGuessIs(player, 'f');
         nextGuessIs(player, 'w');
-        nextGuessIs(player, 'x');
         nextGuessIs(player, 'y');
+        nextGuessIs(player, 'p');
+        nextGuessIs(player, 'v');
+        nextGuessIs(player, 'b');
+        nextGuessIs(player, 'g');
+        nextGuessIs(player, 'k');
+        nextGuessIs(player, 'q');
+        nextGuessIs(player, 'j');
+        nextGuessIs(player, 'x');
         nextGuessIs(player, 'z');
     }
 
