283b9a2f4a9d411b76fa05ec2952233f483d2c80
zach_lysobey <zach@lysobey.com>
2015-04-13 11:12:51 -0400

    have hangman use rstlne then all other guesses alphabetically

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,48 @@
 package com.hangman.players;
+
 import com.hangman.Player;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class YourPlayer implements Player {
-  @Override
-  public char GetGuess(List<Character> currentClue) {
-    return 'a';
-  }
+
+    Queue<Character> bestGuesses = new LinkedList<Character>();
+
+    public YourPlayer() {
+        bestGuesses.add('r');
+        bestGuesses.add('s');
+        bestGuesses.add('t');
+        bestGuesses.add('l');
+        bestGuesses.add('n');
+        bestGuesses.add('e');
+        bestGuesses.add('a');
+        bestGuesses.add('b');
+        bestGuesses.add('c');
+        bestGuesses.add('d');
+        bestGuesses.add('f');
+        bestGuesses.add('g');
+        bestGuesses.add('h');
+        bestGuesses.add('i');
+        bestGuesses.add('j');
+        bestGuesses.add('k');
+        bestGuesses.add('l');
+        bestGuesses.add('m');
+        bestGuesses.add('n');
+        bestGuesses.add('o');
+        bestGuesses.add('p');
+        bestGuesses.add('q');
+        bestGuesses.add('u');
+        bestGuesses.add('v');
+        bestGuesses.add('w');
+        bestGuesses.add('x');
+        bestGuesses.add('y');
+        bestGuesses.add('z');
+    }
+
+    @Override
+    public char GetGuess(List<Character> currentClue) {
+        char guess = bestGuesses.poll();
+        return guess;
+    }
+
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -6,29 +6,76 @@ import static org.junit.Assert.assertEquals;
 
 public class YourPlayerTest {
     @Test
-    public void GuessesAWhenThereAreNoSuccessfulCharactersGuessedYet() {
+    public void GuessesRWhenThereAreNoSuccessfulCharactersGuessedYet() {
         YourPlayer player = new YourPlayer();
-
         char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
-
-        assertEquals('a', guess);
+        assertEquals('r', guess);
     }
 
     @Test
-    public void GuessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotA() {
+    public void GuessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotR() {
         YourPlayer player = new YourPlayer();
-
         char guess = player.GetGuess(Arrays.asList('m', '_', 'n'));
-
-        assertEquals('a', guess);
+        assertEquals('r', guess);
     }
 
     @Test
-    public void GuessesAWhenAIsThereAreAsInTheClueAsWell() {
+    public void DoesNotGuessAWhenAIsInTheClueAsWell() {
         YourPlayer player = new YourPlayer();
-
-        char guess = player.GetGuess(Arrays.asList('_', 'a', '_'));
-
-        assertEquals('a', guess);
+        char guess = player.GetGuess(Arrays.asList('_', 'r', '_'));
+        assertEquals('r', guess);
     }
+
+    @Test
+    public void WillAnswerWithRSTLNEInOrder() {
+        YourPlayer player = new YourPlayer();
+        char guess1 = player.GetGuess(Arrays.asList('_'));
+        assertEquals('r', guess1);
+        char guess2 = player.GetGuess(Arrays.asList('_'));
+        assertEquals('s', guess2);
+        char guess3 = player.GetGuess(Arrays.asList('_'));
+        assertEquals('t', guess3);
+        char guess4 = player.GetGuess(Arrays.asList('_'));
+        assertEquals('l', guess4);
+        char guess5 = player.GetGuess(Arrays.asList('_'));
+        assertEquals('n', guess5);
+        char guess6 = player.GetGuess(Arrays.asList('_'));
+        assertEquals('e', guess6);
+    }
+
+    @Test
+    public void WillAnswerAlphabeticallyAfterTheFirst6Attempts() {
+        YourPlayer player = new YourPlayer();
+        for (int i = 0; i < 6; i++) {
+            player.GetGuess(Arrays.asList('_'));
+        }
+        nextGuessIs(player, 'a');
+        nextGuessIs(player, 'b');
+        nextGuessIs(player, 'c');
+        nextGuessIs(player, 'd');
+        nextGuessIs(player, 'f');
+        nextGuessIs(player, 'g');
+        nextGuessIs(player, 'h');
+        nextGuessIs(player, 'i');
+        nextGuessIs(player, 'j');
+        nextGuessIs(player, 'k');
+        nextGuessIs(player, 'l');
+        nextGuessIs(player, 'm');
+        nextGuessIs(player, 'n');
+        nextGuessIs(player, 'o');
+        nextGuessIs(player, 'p');
+        nextGuessIs(player, 'q');
+        nextGuessIs(player, 'u');
+        nextGuessIs(player, 'v');
+        nextGuessIs(player, 'w');
+        nextGuessIs(player, 'x');
+        nextGuessIs(player, 'y');
+        nextGuessIs(player, 'z');
+    }
+
+    private void nextGuessIs(YourPlayer player, char expectedGuess) {
+        char guess = player.GetGuess(Arrays.asList('_'));
+        assertEquals(expectedGuess, guess);
+    }
+
 }
