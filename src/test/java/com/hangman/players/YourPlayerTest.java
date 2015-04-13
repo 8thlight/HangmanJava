a1f4bee18ca32a733f01064bf1a4f37f4dbe7534
ssi-hu-zoltan-beke <Zoltan.Beke@SurveySampling.com>
2015-04-13 12:26:36 -0400

    YourPlayerTest

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,17 +1,27 @@
 package com.hangman.players;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class YourPlayerTest {
     @Test
     public void GuessesAWhenThereAreNoSuccessfulCharactersGuessedYet() {
         YourPlayer player = new YourPlayer();
+        List<Character> allChars = new ArrayList<>();
 
         char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
+        for (int i = 0; i < 26; i++){
+            char c = (char) (i+ 'a');
+            allChars.add(c);
+        }
 
-        assertEquals('a', guess);
+        assertTrue(allChars.contains(guess));
     }
 
     @Test
