b5db1c1c628346d9f8074d675bf232935be2a9a0
Danny Agapito <danny.agapito@gmail.com>
2015-04-13 11:45:41 -0400

    implemented player and added tests

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,13 +1,28 @@
 package com.hangman.players;
+
 import com.hangman.Player;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class YourPlayer implements Player {
+
+    private Queue<Character> letters;
+
+    public YourPlayer() {
+
+        letters = new LinkedList<Character>(Arrays.asList('e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'));
+    }
+
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+
+      char c;
+
+      do {
+          c = letters.poll();
+      }
+      while (clue.contains(c));
+
+      return c;
   }
 }
