d55490f0e2d4e96d167fa05b6874e17bfd3a2e15
Joe Parker <joe.parker@surveysampling.com>
2015-04-13 12:40:58 -0400

    initial fork and unit test

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,4 +1,5 @@
 package com.hangman.players;
+import com.hangman.HangmanGame;
 import com.hangman.Player;
 
 import java.util.Arrays;
@@ -6,8 +7,39 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class YourPlayer implements Player {
+
+  private LinkedList<Character> guesses;
+  private List<Character> latestClue;
+
+  public static final List<Character> mostCommon2LeastLetters;
+
+  static {
+    mostCommon2LeastLetters = Arrays.asList(
+            'e', 'a', 'r', 'i', 'o', 't', 'n', 's', 'l', 'c', 'u', 'd', 'p', 'm', 'h', 'g',
+            'b', 'f', 'y', 'w', 'k', 'v', 'x', 'z', 'j', 'q');
+  }
+
+  public YourPlayer() {
+    guesses = new LinkedList<Character>();
+    this.SetGuesses(mostCommon2LeastLetters);
+  }
+
+  private void SetGuesses(List<Character> chars) {
+    this.guesses = new LinkedList<Character>();
+    for(char guess : chars) {
+      this.guesses.add(guess);
+    }
+  }
+
   @Override
   public char GetGuess(List<Character> clue) {
-    return 'a';
+    latestClue = clue;
+    if (guesses.size() > 0)
+      return guesses.pop();
+    return 0;
+  }
+
+  public List<Character> GetLatestClue() {
+    return latestClue;
   }
 }
--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,16 +1,39 @@
 package com.hangman.players;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class YourPlayerTest {
+
+    private YourPlayer player;
+
+    @Before
+    public void setUp() {
+        player = new YourPlayer();
+    }
+
     @Test
-    public void AlwaysGuessA() {
-        YourPlayer player = new YourPlayer();
+    public void GetLatestClueShouldReturnNothing() {
+        assertNull(player.GetLatestClue());
+    }
 
-        char guess = player.GetGuess(Arrays.asList('a', 'b', 'c'));
+    @Test
+    public void GetGuessShouldReturnFirstElementInMostCommonLetterList() {
+        char guess = player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertEquals(guess, YourPlayer.mostCommon2LeastLetters.get(0).charValue());
+    }
 
-        assertEquals('a', guess);
+    @Test
+    public void GetLatestClueSHouldMatchInitialClueList() {
+        List <Character> clue = Arrays.asList('_', '_', '_');
+        player.GetGuess(Arrays.asList('_', '_', '_'));
+        assertEquals(clue, player.GetLatestClue());
     }
 }
