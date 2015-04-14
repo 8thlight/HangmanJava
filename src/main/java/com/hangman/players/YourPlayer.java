88a509706c7464c12032dcdd7989bac9efc79f70
Danny Agapito <danny.agapito@gmail.com>
2015-04-14 13:06:06 -0400

    edits

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -2,15 +2,18 @@ package com.hangman.players;
 
 import com.hangman.Player;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class YourPlayer implements Player {
 
-    private Queue<Character> letters;
+    private Queue<Character> lettersMostToLeastUsed;
 
     public YourPlayer() {
 
-        letters = new LinkedList<Character>(Arrays.asList('e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'));
+        lettersMostToLeastUsed = new LinkedList<Character>(Arrays.asList('e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u', 'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'));
     }
 
   @Override
@@ -19,7 +22,7 @@ public class YourPlayer implements Player {
       char c;
 
       do {
-          c = letters.poll();
+          c = lettersMostToLeastUsed.poll();
       }
       while (clue.contains(c));
 
