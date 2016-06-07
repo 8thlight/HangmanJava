4499d6de5153e0d797a2c6bb23339748b789a703
Eric Smith <esmith@8thlight.com>
2016-06-07 11:55:51 -0500

    Pr test for my script

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class YourPlayer implements Player {
     @Override
     public char getGuess(List<Character> currentClue) {
-    return 'a';
-  }
+        return 'b';
+    }
 }
