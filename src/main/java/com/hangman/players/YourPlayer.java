fb5605d6bfc9dbe2b1b6f9297610ef18f09a24b4
Eric Smith <esmith@8thlight.com>
2016-06-07 12:02:17 -0500

    Fix spacing on YourPlayer.java

--- src/main/java/com/hangman/players/YourPlayer.java
+++ src/main/java/com/hangman/players/YourPlayer.java
@@ -1,11 +1,12 @@
 package com.hangman.players;
 
 import com.hangman.Player;
+
 import java.util.List;
 
 public class YourPlayer implements Player {
     @Override
     public char getGuess(List<Character> currentClue) {
-    return 'a';
-  }
+        return 'a';
+    }
 }
