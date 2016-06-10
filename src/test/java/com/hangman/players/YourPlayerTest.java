3c839724e322a4d4034791594afd0955c755c44b
Jason <jgoodman@8thlight.com>
2016-06-10 16:59:50 -0500

    Add guess i and start of random guessing

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -2,10 +2,21 @@ package com.hangman.players;
 
 import org.junit.Test;
 import org.junit.Before;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class YourPlayerTest {
+    public static final List<Character> ALL_CHARACTERS = Collections.unmodifiableList(Arrays.asList(
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z'
+    ));
+
     private YourPlayer player;
 
     @Before
@@ -33,4 +44,29 @@ public class YourPlayerTest {
 
         assertEquals('a', guess);
     }
+
+    @Test
+    public void guessesIWhenEAndAAreInTheClue() {
+        char guess = player.getGuess(Arrays.asList('e', 'a', null));
+
+        assertEquals('i', guess);
+    }
+
+    @Test
+    public void guessesARandomCharacterWhenTheClueContainsEAI() {
+        List<Character> chars = new ArrayList<>(ALL_CHARACTERS);
+        chars.removeAll(Arrays.asList('a', 'e', 'i'));
+        char guess = player.getGuess(Arrays.asList('e', 'a', 'i', null));
+
+        assertTrue(chars.contains(guess));
+    }
+
+    @Test
+    public void doesNotGuessTheSameRandomCharacterTwice() {
+        char guess1 = player.getGuess(Arrays.asList('e', 'a', 'i', null));
+        char guess2 = player.getGuess(Arrays.asList('e', 'a', 'i', null));
+
+        assertNotEquals(guess1, guess2);
+    }
+
 }
