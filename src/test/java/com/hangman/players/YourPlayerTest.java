2689cd04151fa1a7c65daf9635cc3eefa79cda79
Lucas Schwarz <lucas@usamp.com>
2016-06-14 13:17:44 -0700

    SHIP IT

--- src/test/java/com/hangman/players/YourPlayerTest.java
+++ src/test/java/com/hangman/players/YourPlayerTest.java
@@ -1,34 +1,60 @@
 package com.hangman.players;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
-import static org.junit.Assert.assertEquals;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class YourPlayerTest {
-    @Test
-    public void guessesAWhenThereAreNoSuccessfulCharactersGuessedYet() {
-        YourPlayer player = new YourPlayer();
 
-        char guess = player.getGuess(Arrays.asList(null, null, null));
-
-        assertEquals('a', guess);
-    }
 
     @Test
-    public void guessesAWhenThereAreSuccessfulCharactersGuessedThatAreNotA() {
-        YourPlayer player = new YourPlayer();
-
-        char guess = player.getGuess(Arrays.asList('m', null, 'n'));
-
-        assertEquals('a', guess);
+    public void alwaysPickNextPopularChar() {
+	    YourPlayer player = new YourPlayer();
+	    player.setCharStrategy(Arrays.asList(null, null, null));
+	    //note the two most popular characters
+	    List<Character> expectedGuesses = new ArrayList<>();
+      expectedGuesses.add(player.getPopularChars().get(0));
+	    expectedGuesses.add(player.getPopularChars().get(1));
+	    //make two guesses
+      char guess1 = player.getGuess(Arrays.asList(null, null, null));
+      char guess2 = player.getGuess(Arrays.asList(null, null, null));
+	    System.out.println(guess1);
+	    System.out.println(guess2);
+	    //verify those guesses match our popular characters
+      assertEquals(expectedGuesses, player.getAttemptedChars());
     }
 
-    @Test
-    public void guessesAWhenAIsThereAreAsInTheClueAsWell() {
-        YourPlayer player = new YourPlayer();
+		@Test
+    public void chooseStrategyByClueSize() {
+	    YourPlayer player = new YourPlayer();
+	    //make a clue of a given size and feed it to player
+	    List<Character>wordLength3Chars = Arrays.asList(null, null, null);
+	    player.setCharStrategy(wordLength3Chars);
+	    player.getGuess(wordLength3Chars);
 
-        char guess = player.getGuess(Arrays.asList(null, 'a', null));
-
-        assertEquals('a', guess);
+			List<Character>expectedPopularChars = player.wordLength3Chars;
+	    List<Character>actualPopularChars = player.getPopularChars();
+	    //verify they are teh same
+	    assertEquals(expectedPopularChars, actualPopularChars);
     }
+
+    		@Test
+    public void chooseLargestStrategyIfNoMatch() {
+	    YourPlayer player = new YourPlayer();
+
+	    //make a clue of an unsupported size and feed it to player
+	    List<Character>wordLength11Chars = Arrays.asList(null, null, null, null, null, null, null, null, null, null, null);
+	    player.setCharStrategy(wordLength11Chars);
+	    player.getGuess(wordLength11Chars);
+
+			List<Character>expectedPopularChars = player.wordLength10Chars;
+	    List<Character>actualPopularChars = player.getPopularChars();
+	    //verify they are teh same
+	    assertEquals(expectedPopularChars, actualPopularChars);
+    }
+
 }
